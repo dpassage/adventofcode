4b5c8ead7df3cf5616b8b98178799b30cbdd1bd4
David Paschich <dpassage@balveda.com>
2018-01-15 00:01:53 -0800

    Day 20

--- Sources/Day20Elves/main.swift
+++ Sources/Day20Elves/main.swift
@@ -1,26 +1,47 @@
 import Foundation
 
-func presents(n: Int) -> Int {
-    var result = 0
-    for i in 1 ... n {
-        if n % i == 0 {
-            result += i * 10
-        }
-    }
-    return result
-}
+
 
 func firstHouseWithAtLeast(n: Int) -> Int {
-    var result = 0
-    while true {
-        result += 1
-        let delivered = presents(n: result)
-        print("\(result): \(delivered)")
-        if delivered >= n {
-            break
+    let target = n / 10
+
+    var houses = Array<Int>(repeating: 1, count: target)
+
+    for elf in 2..<target {
+        var i = elf
+        while i < target {
+            houses[i] += elf
+            i += elf
         }
     }
-    return result
+    // find first
+    for i in 0..<target {
+        if houses[i] >= target { return i }
+    }
+    return -1
 }
 
-print(firstHouseWithAtLeast(n: 33_100_000))
+print(firstHouseWithAtLeast(n: 33100000))
+
+func elfTwo(limit n: Int) -> Int {
+    let houseCount = n / 11
+    var houses = Array<Int>(repeating: 1, count: houseCount)
+
+    for elf in 1..<houseCount {
+        var house = elf
+        var count = 0
+        while house < houseCount && count < 50 {
+            houses[house] += (elf * 11)
+            house += elf
+            count += 1
+        }
+    }
+
+    // find first
+    for i in 0..<houseCount {
+        if houses[i] >= n { return i }
+    }
+    return -1
+}
+
+print(elfTwo(limit: 33100000))
